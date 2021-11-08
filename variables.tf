c634f2cdf2f91028ba910c7aa47ad5646588c74c
CloudCM IT <lchies@gmail.com>
2021-11-08 10:16:56 -0300

    Add files via upload

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "client_id" {
+    default = "1f5f9d2a-f4f4-4ec9-889b-e026e084225c"
+}
+variable "client_secret" {
+    default = "di-zKprLbyPfXWGV_473zZ3T~WM6Nhw0f~"
+}
+
+variable cluster_name {
+    default = "tf-aksclcm"
+}
+
+variable resource_group_name {
+    default = "tf-aks-rg"
+}
